31dc666b94ef0d5cf37f969db588528e2674ecd1
amyoshi9 <narinarit59@gmail.com>
2025-07-02 23:24:52 +0900

    add comment

--- pendulum/Rigidbody.js
+++ pendulum/Rigidbody.js
@@ -14,7 +14,7 @@ export class Rigidbody {
     this.angularVelocity = 0;
     this.angularAcceleration = 0;
   }
-
+  //重力
   applyGravity(dt) {
     const gravityForce = new THREE.Vector3(0, -this.g * this.mass, 0);
     const totalForce = gravityForce.add(this.externalForce.clone());
@@ -23,11 +23,16 @@ export class Rigidbody {
     this.position.addScaledVector(this.velocity, dt);
     this.externalForce.set(0, 0, 0);
 
+    //着地時の動作
     if (this.isGrounded && this.velocity.y < 0) {
       this.position.y = 2;
-      this.velocity.set(0, 0, 0);
+      this.velocity.y = 0;
+    }
+    if(this.isGrounded){
+       this.applyFriction(0.8, dt);
     }
   }
+   //摩擦力
   applyFriction(coefficient, dt) {
   // 水平方向の速度だけに摩擦力をかける（y方向は重力で制御）
     const frictionForceX = -Math.sign(this.velocity.x) * coefficient * this.mass * this.g;
@@ -41,7 +46,7 @@ export class Rigidbody {
     this.velocity.x = 0;
     }
   }
-
+  //回転力
   applyRotation(dt) {
     const torque = -this.mass * this.g * (this.length / 2) * Math.sin(this.angle);
     const I = (1 / 3) * this.mass * Math.pow(this.length, 2);
@@ -50,7 +55,6 @@ export class Rigidbody {
     this.angle += this.angularVelocity * dt;
     this.angularVelocity *= 0.999;
     //console.log("angle:", this.angle, "ω:", this.angularVelocity, "α:", this.angularAcceleration);
-    this.applyFriction(0.8, dt);
 
   }
   setGrounded(flag) {
